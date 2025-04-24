510c47573dbf7e99434a8328e834f1391ebef879
Jass <136769749+frontendjass@users.noreply.github.com>
2025-04-24 13:02:29 -0500

    finished project

--- Final Project SPR-2025/Inventory.hpp
+++ Final Project SPR-2025/Inventory.hpp
@@ -1,13 +1,35 @@
-//
-//  Inventory.hpp
-//  Final Project SPR-2025
-//
-//  Created by Juan Sosa on 4/15/25.
-//
+// This is the Inventory class
 
 #ifndef Inventory_hpp
 #define Inventory_hpp
 
-#include <stdio.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Inventory {
+private:
+    int itemID;
+    string itemName;
+    string itemDescription;
+    double itemPrice;
+
+public:
+    Inventory(); // Default constructor
+    Inventory(int ID, string name, string description, double price); // Parameter constructor
+    void Display() const;
+
+    // Getters
+    int getID() const;
+    string getName() const;
+    string getDescription() const;
+    double getPrice() const;
+
+    // Setters (optional if needed)
+    void setID(int ID);
+    void setName(const string& name);
+    void setDescription(const string& desc);
+    void setPrice(double price);
+};
 
 #endif /* Inventory_hpp */
--- Final Project SPR-2025/InventoryUtils.hpp
+++ Final Project SPR-2025/InventoryUtils.hpp
@@ -1,13 +1,20 @@
-//
-//  InventoryUtils.hpp
-//  Final Project SPR-2025
-//
-//  Created by Juan Sosa on 4/17/25.
-//
+// These are the function prototypes separated into its own file
 
 #ifndef InventoryUtils_hpp
 #define InventoryUtils_hpp
 
-#include <stdio.h>
+#include <vector>
+#include <string>
+#include "Inventory.hpp"
+
+vector<Inventory> ImportInventory(); // This is a vector(like an array) of Inventory type objects
+void PrintMenu(const vector<Inventory>& inventoryList, string nameKey, int key);
+void ListAllItems(const vector<Inventory>& inventoryList);
+void ListAscendingID(const vector<Inventory>& inventoryList);
+vector<int> SearchByName(const vector<Inventory>& inventoryList, const string& nameKey); // Returns the index of the item
+int SearchByID(const vector<Inventory>& inventoryList, int key); // Returns the index of the item
+void ListByName(const vector<Inventory>& inventoryList);
+void ListByPrice(const vector<Inventory>& inventoryList);
+
 
 #endif /* InventoryUtils_hpp */
